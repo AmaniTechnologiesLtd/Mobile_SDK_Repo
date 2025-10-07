543c4e5a5a8bd3a64a0d08c9cafae640e791666a
Münir Ketizmen <78314645+munir-amani@users.noreply.github.com>
2025-10-07 17:19:38 +0300

    v3.4.19

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let CoreVersion = "3.4.18"
-let CoreChecksum = "62e6af099988972e7e9845eb4b7ad51c34985ddf4c87672e0ca30c25662c8c84"
+let CoreVersion = "3.4.19"
+let CoreChecksum = "59fb82c50e0b4e3963d8ea5b91ba61361d70bcdd43221ee5af98d31ff5976e20"
 
 let VideoVersion = "2.0.3"
 let VideoCheckSum = "8dcf51f00f24e599feec1cb92e4e0631216494335a431debec1a6b597e3110ad"
