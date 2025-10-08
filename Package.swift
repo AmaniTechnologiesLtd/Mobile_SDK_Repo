a789dc7234684b177b3a96dd0da7f201d49c3c55
Münir Ketizmen <78314645+munir-amani@users.noreply.github.com>
2025-10-08 16:53:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .library(
             name:"AmaniVideoSDK",
             targets: ["AmaniVideoBundle"]
-        )
+        ),
         .library(
             name: "AmaniBioMatch",
             targets: ["AmaniBioMatchBundle"]
